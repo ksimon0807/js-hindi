de3e288accfa73ef3308c763b7e48103e16a526c
ksimon0807 <mujjawal774@gmail.com>
2024-10-18 17:44:36 +0000

    mathss and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(3));
+
+const otherNumber = 230.221
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000005464
+console.log(hundreds.toLocaleString('en-US'));
+
+// ++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-65));
+console.log(Math.ceil(516.51));
+console.log(Math.ceil(516.51));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min =  10
+const max = 30
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
