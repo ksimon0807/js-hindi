0751e5548076b2ebc6fa5266dce53154d4421137
ksimon0807 <mujjawal774@gmail.com>
2024-10-19 17:43:18 +0000

    functions and scopes

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,24 @@
+// var c = 300
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    // c = 30                
+      console.log("Inner : ", a);
+    function add(){
+        let a = 56
+        console.log("Innermost : ",a);
+        
+    }
+      
+}
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
